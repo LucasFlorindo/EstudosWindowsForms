be21252ad7f0d94e652fa945d13cd05db5687dba
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-04-24 10:08:38 -0300

    this.close

--- CursoWindowsFormsAlura/CursoWindowsFormsAlura/Hello World.cs
+++ CursoWindowsFormsAlura/CursoWindowsFormsAlura/Hello World.cs
@@ -22,7 +22,7 @@ namespace CursoWindowsFormsAlura
 
             try
             {
-                Application.Exit();
+                this.Close();
 
             }catch (Exception m)
             {
