f0e8e4868aca1564859ddce4029274cce333c9e9
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-05-02 09:43:41 -0300

    Messagebox mousedown

--- CURSO3ProjetoBanco/CursoWindowsForms/CursoWindowsForms/Program.cs
+++ CURSO3ProjetoBanco/CursoWindowsForms/CursoWindowsForms/Program.cs
@@ -16,7 +16,7 @@ namespace CursoWindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_CapturaMouse());
+            Application.Run(new frm_MouseCursor());
         }
     }
 }
---
+++ CURSO3ProjetoBanco/CursoWindowsForms/CursoWindowsForms/frm_MenuFlutuante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class frm_MenuFlutuante : Form
+    {
+        public frm_MenuFlutuante()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_MenuFlutuante_MouseDown(object sender, MouseEventArgs e)
+        {
+            string str1 = e.Button.ToString();
+        }
+    }
+}
