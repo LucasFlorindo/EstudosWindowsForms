9717682e198e2197551989cb75620ecc1fad6db4
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-05-03 10:43:08 -0300

    Initializing Tabpage

--- CURSO3ProjetoBanco/CursoWindowsForms/CursoWindowsForms/frm_MenuFlutuante.cs
+++ CURSO3ProjetoBanco/CursoWindowsForms/CursoWindowsForms/frm_MenuFlutuante.cs
@@ -27,17 +27,36 @@ namespace CursoWindowsForms
                 //MessageBox.Show("Relative click position: ("+PosicaoX.ToString()+","+ PosicaoY.ToString()+")");
 
                 var ContextMenu = new ContextMenuStrip();
+                var vToolTip001 = DesnehaItemMenu("Item do Menu 1", "key");
+                var vToolTip002 = DesnehaItemMenu("Item do Menu 2", "Frm_ValidaSenha");
 
-                ContextMenu.Items.Add(DesnehaItemMenu("Item do Menu 1"));
-                ContextMenu.Items.Add(DesnehaItemMenu("Item do Menu 2"));
+                ContextMenu.Items.Add(vToolTip001);
+                ContextMenu.Items.Add(vToolTip002);
                 ContextMenu.Show(this, new Point(e.X, e.Y));
+
+                vToolTip001.Click += new System.EventHandler(vToolTip001_Click);
+                vToolTip002.Click += new System.EventHandler(vToolTip002_Click);
 
             }
 
-            ToolStripMenuItem DesnehaItemMenu(string text)
+            void vToolTip001_Click(object sender1, EventArgs e1)
+            {
+
+            }
+
+            void vToolTip002_Click(object sender1, EventArgs e1)
+            {
+
+            }
+
+            ToolStripMenuItem DesnehaItemMenu(string text, string nomeImagem)
             {
                 var vToolTip = new ToolStripMenuItem();
                 vToolTip.Text = text;
+
+                Image MyImage = (Image)global::CursoWindowsForms.Properties.Resources.ResourceManager.GetObject(nomeImagem);
+                vToolTip.Image = MyImage;
+
                 return vToolTip;
             }
         }
