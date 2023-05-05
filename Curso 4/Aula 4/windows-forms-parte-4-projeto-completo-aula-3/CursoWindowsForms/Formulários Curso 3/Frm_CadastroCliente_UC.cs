214cf53a6344ddc76e8f36d9dbd58c4d2d992f6b
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-05-05 13:28:11 -0300

    Creating methods and exception for every textbox

---
+++ Curso 4/Aula 4/windows-forms-parte-4-projeto-completo-aula-3/CursoWindowsForms/Formulários Curso 3/Frm_CadastroCliente_UC.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CursoWindowsFormsBiblioteca.Classes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_CadastroCliente_UC : UserControl
+    {
+        public Frm_CadastroCliente_UC()
+        {
+            InitializeComponent();
+            Grp_Codigo.Text = "Código";
+            Grp_DadosPessoais.Text = "Dados Pessoais";
+            Grp_Endereco.Text = "Endereço";
+            Grp_Outros.Text = "Outros";
+            Lbl_Bairro.Text = "Bairro";
+            Lbl_CEP.Text = "CEP";
+            Lbl_Complemento.Text = "Complemento";
+            Lbl_CPF.Text = "CPF";
+            Lbl_Estado.Text = "Estado";
+            Lbl_Logradouro.Text = "Logradouro";
+            Lbl_NomeCliente.Text = "Nome";
+            Lbl_NomeMae.Text = "Nome da Mãe";
+            Lbl_NomePai.Text = "Nome do Pai";
+            Lbl_Profissao.Text = "Profissão";
+            Lbl_RendaFamiliar.Text = "Renda Familiar";
+            Lbl_Telefone.Text = "Telefone";
+            Lbl_Cidade.Text = "Cidade";
+            Chk_TemPai.Text = "Pai desconhecido";
+            Rdb_Masculino.Text = "Masculino";
+            Rdb_Feminino.Text = "Feminino";
+            Rdb_Indefinido.Text = "Indefinido";
+            Grp_Genero.Text = "Genero";
+
+            Cmb_Estados.Items.Clear();
+            Cmb_Estados.Items.Add("Acre (AC)");
+            Cmb_Estados.Items.Add("Alagoas(AL)");
+            Cmb_Estados.Items.Add("Amapá(AP)");
+            Cmb_Estados.Items.Add("Amazonas(AM)");
+            Cmb_Estados.Items.Add("Bahia(BA)");
+            Cmb_Estados.Items.Add("Ceará(CE)");
+            Cmb_Estados.Items.Add("Distrito Federal(DF)");
+            Cmb_Estados.Items.Add("Espírito Santo(ES)");
+            Cmb_Estados.Items.Add("Goiás(GO)");
+            Cmb_Estados.Items.Add("Maranhão(MA)");
+            Cmb_Estados.Items.Add("Mato Grosso(MT)");
+            Cmb_Estados.Items.Add("Mato Grosso do Sul(MS)");
+            Cmb_Estados.Items.Add("Minas Gerais(MG)");
+            Cmb_Estados.Items.Add("Pará(PA)");
+            Cmb_Estados.Items.Add("Paraíba(PB)");
+            Cmb_Estados.Items.Add("Paraná(PR)");
+            Cmb_Estados.Items.Add("Pernambuco(PE)");
+            Cmb_Estados.Items.Add("Piauí(PI)");
+            Cmb_Estados.Items.Add("Rio de Janeiro(RJ)");
+            Cmb_Estados.Items.Add("Rio Grande do Norte(RN)");
+            Cmb_Estados.Items.Add("Rio Grande do Sul(RS)");
+            Cmb_Estados.Items.Add("Rondônia(RO)");
+            Cmb_Estados.Items.Add("Roraima(RR)");
+            Cmb_Estados.Items.Add("Santa Catarina(SC)");
+            Cmb_Estados.Items.Add("São Paulo(SP)");
+            Cmb_Estados.Items.Add("Sergipe(SE)");
+            Cmb_Estados.Items.Add("Tocantins(TO)");
+
+            Tls_Principal.Items[0].ToolTipText = "Incluir na base de dados um novo cliente";
+            Tls_Principal.Items[1].ToolTipText = "Capturar um cliente já cadastrado na base";
+            Tls_Principal.Items[2].ToolTipText = "Atualize o cliente já existente";
+            Tls_Principal.Items[3].ToolTipText = "Apaga o cliente selecionado";
+            Tls_Principal.Items[4].ToolTipText = "Limpa dados da tela de entrada de dados";
+
+
+        }
+
+        private void Chk_TemPai_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Chk_TemPai.Checked)
+            {
+                Txt_NomePai.Enabled = false;
+            }
+            else 
+            { 
+                Txt_NomePai.Enabled = true; 
+            }
+        }
+
+        private void novoToolStripButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Cliente.Unit C = new Cliente.Unit();
+                C.Id = Txt_Codigo.Text;
+                C.ValidaClasse();
+                MessageBox.Show("Classe foi inicializada sem erros", "ByteBank", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (ValidationException Ex)
+            {
+                MessageBox.Show(Ex.Message, "ByteBank", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }          
+        }
+
+        private void abrirToolStripButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Efetuei um clique sobre o botão ABRIR");
+        }
+
+        private void salvarToolStripButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Efetuei um clique sobre o botão SALVAR");
+        }
+
+        private void ApagatoolStripButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Efetuei um clique sobre o botão EXCLUIR");
+        }
+
+        private void LimpartoolStripButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Efetuei um clique sobre o botão LIMPAR");
+        }
+    }
+}
