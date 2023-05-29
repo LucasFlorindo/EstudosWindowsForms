4881816c834d4a805d7da63fbd464f483799aee0
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-05-29 15:35:23 -0300

    SQL commands w c#

---
+++ Curso 6/CursoWindowsForms (3)/CursoWindowsForms/CursoWindowsForms/frm_FileBrowserToGetPath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class frm_FileBrowserToGetPath : Form
+    {
+        public frm_FileBrowserToGetPath()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            string folderPath = folderBrowserDialog1.SelectedPath;
+            textBox1.Text = folderPath;
+        }
+    }
+}
---
+++ Curso 6/CursoWindowsForms (3)/CursoWindowsForms/CursoWindowsFormsBiblioteca/Databases/FicharioDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class FicharioDB
+    {
+
+        public string mensagem;
+        public bool status;
+        public string tabela;
+        public LocalDBClass db;
+
+        public FicharioDB(string Tabela)
+        {
+
+            status = true;
+            try
+            {
+                db = new LocalDBClass();
+                tabela = Tabela;
+                mensagem = "Conexão com a Tabela criada";
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+            }
+        }
+
+
+    }
+}
